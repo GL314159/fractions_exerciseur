06524b44d3283a6184e0618ca13a17203d35e5fd
GL314159 <114300572+GL314159@users.noreply.github.com>
2025-05-29 09:36:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from fractions import Fraction
 import pandas as pd
 import math
 
-NB_QUESTIONS = 8
-
-
 
 
 st.markdown(
@@ -16,6 +13,9 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+
+
+
 # --- Niveaux disponibles ---
 NIVEAUX = {
     "1 — Simplification uniquement": ["="],
@@ -24,7 +24,6 @@ NIVEAUX = {
 }
 
 previous_level = st.session_state.get("niveau_selectionne", None)
-#niveau = st.selectbox("🎯 Choisis un niveau", list(NIVEAUX.keys()))
 
 st.markdown(
     """
@@ -41,9 +40,18 @@ with col2:
     niveau = st.selectbox("", list(NIVEAUX.keys()))
 with col1:
     st.markdown("**Choisissez un niveau**")
+
 st.session_state.niveau_selectionne = niveau
 
 
+
+
+
+# Définir le nombre de questions selon le niveau
+if niveau == "3 — Mélange":
+    NB_QUESTIONS = 10
+else:
+    NB_QUESTIONS = 8
 
 
 
@@ -66,7 +74,7 @@ def generer_question():
         return (a, b, "=", 0, 0)
     else:
         a, b, c, d = 2, 2, 2, 2
-        while math.gcd(c, d) > 1 or math.gcd(a, b) > 1  or (a, b) in st.session_state.deja_eu:
+        while math.gcd(c, d) > 1 or math.gcd(a, b) > 1   or   (a, b) in st.session_state.deja_eu   or  b*d==1:
             a = random.randint(1, 6) # pas besoin de numérateurs très grands
             b = random.randint(1, 10)
             c = random.randint(1, 6) # pas besoin de numérateurs très grands
@@ -84,7 +92,9 @@ def calculer_resultat(a, b, op, c, d):
     elif op == "=": return Fraction(a, b)
 
 def latex_fraction(frac):
-    return f"\\dfrac{{\\mathsf{{{frac.numerator}}}}}{{\\mathsf{{{frac.denominator}}}}}"
+    if frac.denominator != 1:
+        return f"\\dfrac{{\\mathsf{{{frac.numerator}}}}}{{\\mathsf{{{frac.denominator}}}}}"
+    return f"\\mathsf{{{frac.numerator}}}"
 
 def latex_frac(a, b):
     if b == 1:
@@ -92,7 +102,7 @@ def latex_frac(a, b):
     else:
         return f"\\dfrac{{\\mathsf{{{a}}}}}{{\\mathsf{{{b}}}}}"
 
-def explication_operation(a, b, op, c, d):
+def explication_operation(a, b, op, c, d, num, den): #num, den = ce qu'a rentré l'utilisateur
     f1, f2 = Fraction(a, b), Fraction(c, d) if d != 0 else Fraction(1, 1)
 
     if op == "=":
@@ -105,30 +115,46 @@ def explication_operation(a, b, op, c, d):
         return
 
     if op in ["+", "-"]:
-        operation = "addition" if op == "+" else "soustraction"
-        st.markdown(f"Pour faire une **{operation}**, il faut mettre les deux fractions au même dénominateur.")
-        M = math.lcm(b, d)
-        st.markdown(f"Ici, le plus petit dénominateur commun est {M}.")
-        #### quid si les fractions ne sont pas simplifiées au départ ? ***
-        st.markdown(rf"On amplifie chaque fraction : $\quad\displaystyle {latex_frac(a,b)} = {latex_frac(a*math.lcm(b, d)//b, M)}\quad$ et $\quad\displaystyle {latex_frac(c, d)}= {latex_frac(c*math.lcm(b, d)//d, M)}\quad$")
         result = f1 + f2 if op == "+" else f1 - f2
-        st.markdown(rf"On déduit donc que $\quad\displaystyle {latex_fraction(f1)} \; {op} \; {latex_fraction(f2)} \;=\; {latex_frac(a*math.lcm(b, d)//b, M)} + {latex_frac(c*math.lcm(b, d)//d, M)} \;=\; {latex_frac(a*math.lcm(b, d)//b  +  c*math.lcm(b, d)//d, M)}$")
-        if result.denominator != M:
-            st.markdown(rf"Finalement, on simplifie la fraction obtenue : $\quad\displaystyle {latex_frac(a*math.lcm(b, d)//b  +  c*math.lcm(b, d)//d, M)} \;=\; {latex_fraction(result)}$")
+        if Fraction(num, den) == result and den != result.denominator:
+            st.markdown(f"C'est presque juste ! Vous avez uniquement oublié de simplifier la fraction.")
+        else:
+            operation = "addition" if op == "+" else "soustraction"
+            st.markdown(f"Pour faire une **{operation}**, il faut mettre les deux fractions au même dénominateur.")
+            M = math.lcm(b, d)
+            st.markdown(f"Ici, le plus petit dénominateur commun est {M}.")
+            #### quid si les fractions ne sont pas simplifiées au départ ? ***
+            st.markdown(rf"On amplifie chaque fraction : $\quad\displaystyle {latex_frac(a,b)} = {latex_frac(a*math.lcm(b, d)//b, M)}\quad$ et $\quad\displaystyle {latex_frac(c, d)}= {latex_frac(c*math.lcm(b, d)//d, M)}\quad$")
+            st.markdown(rf"On déduit donc que $\quad\displaystyle {latex_fraction(f1)} \; {op} \; {latex_fraction(f2)} \;=\; {latex_frac(a*math.lcm(b, d)//b, M)} + {latex_frac(c*math.lcm(b, d)//d, M)} \;=\; {latex_frac(a*math.lcm(b, d)//b  +  c*math.lcm(b, d)//d, M)}$")
+            if result.denominator != M:
+                st.markdown(rf"Finalement, on simplifie la fraction obtenue : $\quad\displaystyle {latex_frac(a*math.lcm(b, d)//b  +  c*math.lcm(b, d)//d, M)} \;=\; {latex_fraction(result)}$")
+
     elif op == "*":
         result = Fraction(a * c, b * d)
-        st.markdown("Pour **multiplier** deux fractions, on multiplie les numérateurs et les dénominateurs :")
-        st.markdown(rf"$\displaystyle {latex_frac(a,b)} \times {latex_frac(c,d)}  \;=\;  {latex_fraction(result)}$")
+        if Fraction(num, den) == result and den != result.denominator:
+            st.markdown(f"C'est presque juste ! Vous avez uniquement oublié de simplifier la fraction.")
+        else:
+            st.markdown("Pour **multiplier** deux fractions, on multiplie les numérateurs et les dénominateurs :")
+            st.markdown(rf"$\displaystyle {latex_frac(a,b)} \cdot {latex_frac(c,d)}  \;=\;  {latex_fraction(result)}$")
     elif op == ":":
         result = Fraction(a * d, b * c)
-        st.markdown("Diviser par une fraction, c’est multiplier par son inverse :")
-        st.markdown(rf"$\displaystyle {latex_frac(a,b)} \div {latex_frac(c, d)}  \;=\;  \dfrac{{{a}}}{{{b}}} \cdot \dfrac{{{d}}}{{{c}}} = {latex_fraction(result)}$")
+        if Fraction(num, den) == result and den != result.denominator:
+            st.markdown(f"C'est presque juste ! Vous avez uniquement oublié de simplifier la fraction.")
+        else:
+            st.markdown("Diviser par une fraction, c’est multiplier par son inverse :")
+            st.markdown(rf"$\displaystyle {latex_frac(a,b)} \div {latex_frac(c, d)}  \;=\;  \dfrac{{{a}}}{{{b}}} \cdot \dfrac{{{d}}}{{{c}}} = {latex_fraction(result)}$")
+
+
+
+
 
 # --- Initialisation ---
 if "question" not in st.session_state:
     st.session_state.question = generer_question()
 if "question_num" not in st.session_state:
     st.session_state.question_num = 1
+if "nb_questions" not in st.session_state:
+    st.session_state.nb_questions = NB_QUESTIONS
 if "score" not in st.session_state:
     st.session_state.score = 0
 if "historique" not in st.session_state:
@@ -142,30 +168,58 @@ if previous_level and niveau != previous_level:
     st.session_state.score = 0
     st.session_state.historique = []
     st.session_state.correction_validee = False
+    st.session_state.deja_eu = []
+    st.session_state.nb_questions = NB_QUESTIONS  # ← à ajouter ici
     st.session_state.question = generer_question()
     st.rerun()
 
 
+
+
 # --- Fin de quiz ---
-if st.session_state.question_num > NB_QUESTIONS:
+if st.session_state.question_num > st.session_state.nb_questions:
     st.title("🎉 Exercice terminé !")
-    st.success(f"🏁 Score final : **{st.session_state.score} / {NB_QUESTIONS}**")
+    st.success(f"🏁 Score final : **{st.session_state.score} / {st.session_state.nb_questions}**")
     df = pd.DataFrame(st.session_state.historique)
     st.dataframe(df)
-    csv = df.to_csv(index=False).encode("utf-8")
-    st.download_button("📥 Télécharger les résultats", csv, "resultats_fractions.csv", "text/csv")
+    col1, col2 = st.columns([3, 1])
+
+    with col1:
+        csv = df.to_csv(index=False).encode("utf-8")
+        st.download_button("📥 Télécharger les résultats", csv, "resultats_fractions.csv", "text/csv")
+
+    with col2:
+        if st.button("🔄 Recommencer"):
+            # Réinitialiser tous les éléments de session
+            st.session_state.question_num = 1
+            st.session_state.score = 0
+            st.session_state.historique = []
+            st.session_state.correction_validee = False
+            st.session_state.deja_eu = []
+            st.session_state.question = generer_question()
+            st.rerun()
+
     st.stop()
+
+
+
+
+
+
+
+
+
 
 # --- Affichage de la question ---
 a, b, op, c, d = st.session_state.question
 
-progress = (st.session_state.question_num - 1) / NB_QUESTIONS * 100
+progress = (st.session_state.question_num - 1) / st.session_state.nb_questions * 100
 
 st.markdown(
     f"""
     <div style='display: flex; align-items: center; gap: 1em;'>
         <div style='font-size: 1rem; white-space: nowrap;'>
-            Question {st.session_state.question_num} / {NB_QUESTIONS}
+            Question {st.session_state.question_num} sur {st.session_state.nb_questions}
         </div>
         <div style='flex-grow: 1; background: #eee; height: 10px; border-radius: 5px;'>
             <div style='width: {progress}%; background: #2b8fe5; height: 100%; border-radius: 5px;'></div>
@@ -177,7 +231,7 @@ st.markdown(
 
 
 st.markdown("<div style='margin-top: 0px;'></div>", unsafe_allow_html=True)
-st.info(f"Score actuel : {st.session_state.score} / {st.session_state.question_num - 1}")
+st.info(f"Score actuel : {st.session_state.score} sur {st.session_state.question_num - 1}")
 
 op_map = {"+": "+", "-": "-", "*": r"\cdot", ":": r"\div", "=": "="}
 if op == "=":
@@ -201,9 +255,11 @@ if submit and not already_corrected:
             rep_utilisateur = Fraction(num, den)
         else:
             rep_utilisateur = Fraction(int(reponse.strip()), 1)
+            num = rep_utilisateur.numerator
+            den = 1
 
         resultat = calculer_resultat(a, b, op, c, d)
-        est_correct = rep_utilisateur == resultat
+        est_correct = (rep_utilisateur == resultat and math.gcd(num,den)==1)
 
         if est_correct:
             st.success("✅ Réponse correcte ! Bravo !")
@@ -212,7 +268,7 @@ if submit and not already_corrected:
             st.error(f"❌ Réponse incorrecte.  \n**La réponse correcte est** $\quad\\displaystyle {latex_fraction(resultat)}$")
             #  deux espaces avant le \n !!!
             with st.expander("💡 Explication détaillée", expanded=False):
-                explication_operation(a, b, op, c, d)
+                explication_operation(a, b, op, c, d, num, den)
 
         st.session_state.historique.append({
             "Niveau": niveau,
@@ -226,6 +282,11 @@ if submit and not already_corrected:
     except:
         st.warning("❗ Format invalide. Exemple : `3/4` ou `2`.")
 
+
+
+
+
+
 # --- Suivant ---
 if st.button("➡️ Question suivante", use_container_width=True):
     st.session_state.question_num += 1
@@ -238,7 +299,7 @@ if st.button("➡️ Question suivante", use_container_width=True):
 
 
 
-
+# --- Nombre de visites ---
 def get_visite_count():
     try:
         with open("visites.txt", "r") as f:
@@ -262,7 +323,7 @@ else:
 
 st.markdown(
     f"<div style='text-align: right; font-size: 0.2em; color: grey; margin-top: 4em;'>"
-    rf"Développé par G. L. (Bussigny, 2025)   <span style='display:inline-block; width:20px;'></span>    Nombre de visites : {total}"
+    rf"Développé par G. Leterrier (Gymnase de Bussigny, 2025)   <span style='display:inline-block; width:20px;'></span>    Nombre de visites : {total}"
     f"</div>",
     unsafe_allow_html=True
 )
